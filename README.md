f6c8caaa80e01695edde42b92c974d640b3b2f53
Duncan Roze <duncan@ttc.me>
2016-03-30 11:19:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+TPSf
+====
+
+A Symfony project created on March 30, 2016, 10:08 am.
---
+++ src/HyperBookBundle/HyperBookBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HyperBookBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HyperBookBundle extends Bundle
+{
+}
